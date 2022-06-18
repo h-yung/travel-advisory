810db8d9f4f60df81f2f5f89cef9c6426d45c620
h-yung <helen.yung.h@gmail.com>
2022-06-18 08:59:51 -0400

    file cleanup

--- react-crud/src/http-common.js
+++ react-crud/src/http-common.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+
 export default axios.create ({
-    baseURL: 'http://localhost:8080/api', //change to rest api
-        "Content-Type": "application/json"
+    baseURL: 'http://localhost:8080/api', //rest api
+        // "Content-Type": "application/json"
     }
 );
